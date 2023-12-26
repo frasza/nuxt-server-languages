aeb4d0654b870e59b0e34141ba617cee78f8fb5c
frasza <zan.fraas@gmail.com>
2023-12-26 18:15:07 +0100

    add limit option

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-const limit = ref(10)
+const limit = ref(5)
 </script>
 
 <template>
-  <div>
-    <Languages :limit="10" />
+  <div class="flex flex-col max-w-4xl p-10">
+    <div class="pb-10 font-semibold">
+      <label
+        >Limit: <input v-model="limit" type="number" class="w-10" min="0"
+      /></label>
+    </div>
+    <hr class="pb-10" />
+    <Languages :limit="limit" />
   </div>
 </template>
--- components/Languages.server.vue
+++ components/Languages.server.vue
@@ -1,26 +1,19 @@
 <script lang="ts" setup>
 import { sql } from '@vercel/postgres'
 
-const { limit } = withDefaults(
-  defineProps<{
-    limit: number
-  }>(),
-  {
-    limit: 10,
-  },
-)
+const { limit } = defineProps<{
+  limit: number
+}>()
 
 const { rows: languages } =
   await sql`SELECT * FROM languages ORDER BY RANDOM() LIMIT ${limit}`
 </script>
 
 <template>
-  <div class="flex justify-center items-center max-w-4xl pt-10">
-    <ul>
-      <li v-for="lang in languages" :key="lang.id" class="pb-10">
-        <span>{{ lang.name }}</span>
-        <img :src="lang.image" class="w-40" />
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li v-for="lang in languages" :key="lang.id" class="pb-10">
+      <span>{{ lang.name }}</span>
+      <img :src="lang.image" class="w-40" />
+    </li>
+  </ul>
 </template>
